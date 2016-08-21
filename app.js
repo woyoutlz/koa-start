8fdb8a280e3596413a70acb867cbbbbb7255ae3e
woyoutlz <yundkyy@qq.com>
2016-08-21 11:50:00 +0800

    init my koa-start app

---
+++ app.js
@@ -0,0 +1,16 @@
+var koa = require('koa');
+var app = koa();
+var render = require('koa-swig');
+var path = require("path")
+app.context.render = render({
+  root: path.join(__dirname, 'views'),
+  autoescape: true,
+  cache: false, // disable, set to false 
+  ext: 'html',
+  varControls:['{{ ', ' }}'] 
+});
+app.use(function *(){
+  yield this.render('index',{name:"yangyu"});
+});
+
+app.listen(3000);
