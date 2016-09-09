e3442c47a23f98b5b5789d8f649236f628830d82
woyoutlz <yundkyy@qq.com>
2016-09-09 20:23:27 +0800

    add static

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var koa = require('koa');
 var app = koa();
+var serve = require('koa-static');
 var render = require('koa-swig');
 var router = require("./route/route")
 var path = require("path")
+app.use(serve('static'));
 app.context.render = render({
   root: path.join(__dirname, 'views'),
   autoescape: true,
@@ -15,5 +17,3 @@ app
   .use(router.allowedMethods());
 
 app.listen(3000);
-
-
